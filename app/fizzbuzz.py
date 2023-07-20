06622cf1dfe2d15f9908be161b574e99984240b9
brett rosenberg <brett.rosenberg@pediatrix.net>
2023-07-20 09:32:22 -0400

    Returns value when number is not divisble by 3 or 5

--- app/fizzbuzz.py
+++ app/fizzbuzz.py
@@ -4,5 +4,7 @@ def calculate(number):
         return "Fizz"
     elif number == 5:
         return "Buzz"
-    else:
+    elif number % 15 ==0:
         return "FizzBuzz"
+    else:
+        return 1
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -15,3 +15,8 @@ def test_calculate_input_of_multiple_of_three_and_five():
     value = calculate(15)
 
     assert "FizzBuzz" == value
+
+def test_calculate_input_of_one():
+    value = calculate(1)
+
+    assert 1 == value
