378c9950ff65df0ba5c8d154e823bdfba539ba53
brett rosenberg <brett.rosenberg@pediatrix.net>
2023-07-21 10:11:33 -0400

    Returns Buzz when input is five

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,3 +5,8 @@ def test_calculate_input_of_three_returns_fizz():
     value = calculate(3)
 
     assert "Fizz" == value
+
+def test_calculate_input_of_five_returns_buzz():
+    value = calculate(5)
+
+    assert "Buzz" == value
