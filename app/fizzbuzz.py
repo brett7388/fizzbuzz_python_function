4be792f8eb9473188ff513505b8d3f79d2b927e9
brett rosenberg <brett.rosenberg@pediatrix.net>
2023-07-25 11:15:48 -0400

    Returns nothing

--- app/fizzbuzz.py
+++ app/fizzbuzz.py
@@ -1,6 +1,11 @@
 
 def calculate(number):
+    if number % 15 == 0:
+        return "FizzBuzz"
     if number == 3:
         return "Fizz"
     if number == 5:
         return "Buzz"
+    else:
+        return 1
+    
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -10,3 +10,13 @@ def test_calculate_input_of_five_returns_buzz():
     value = calculate(5)
 
     assert "Buzz" == value
+
+def test_calculate_input_of_three_and_five_returns_fizzbuzz():
+    value = calculate(15)
+
+    assert "FizzBuzz" == value
+
+def test_caluclate_divisble_of_one():
+    value = calculate(1)
+
+    assert 1 == value
